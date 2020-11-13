dfc3d3719e13649deb555269ec43fba4377ced39
Mike <mike.dilley@gmail.com>
2020-11-12 23:23:03 -0600

    Add counter and h1 click handler in Users

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,11 +1,23 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const Users = () => {
   const router = useRouter();
+  const [count, setCount] = useState(0);
+
+  // This component demos fast refresh and router query string params
+  const handleClick = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
 
   console.log(router);
 
-  return <h1>This is user {router.query.id}</h1>;
+  return (
+    <h1 onClick={handleClick}>
+      This is user's ID is {router.query.id} and the count is {count}
+    </h1>
+  );
 };
 
 export default Users;
